a19d072dc366e6dd2ad8664e7085bac11f02a4de
johnny1786 <jovialjohn4054@gmail.com>
2025-07-24 20:23:46 +0530

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
 });
 
 //C:\Users\john4\Documents\GitHub\portfolio
